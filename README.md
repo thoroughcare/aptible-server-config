88dad13390217e2d206a17c545efebca27d26935
Lance Jordan <djlancejordan@gmail.com>
2017-03-23 08:05:34 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Easy Configs For Aptible Sh Sessions
+
+This will setup your shell environment with Vim, Tmux, Less, and will install Pry into Gemfile for server code debugging.
---
+++ console_setup.sh
@@ -0,0 +1,21 @@
+# For easier working with aptible ssh rails console
+apt-get install vim -y
+apt-get install tmux -y
+apt-get install less -y
+echo "gem 'pry-rails'" >> /app/Gemfile
+echo "gem 'pry-awesome_print'" >> /app/Gemfile
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir ~/.vim/colors/
+wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O ~/.vim/colors/molokai.vim
+wget https://gist.githubusercontent.com/lacostenycoder/779a78eb32ba7607440eb9bdaf4dfb81/raw/a8b9aeb57788434d82b5068ee002c8433c66d38e/.vimrc-rails -O ~/.vimrc
+# install vim plugins before patching ruby syntax
+vim --cmd '' \
+ -c 'PluginInstall' \
+ -c 'qa!'
+# patched syntax file for vim 7.3 on old linux for server
+wget https://gist.githubusercontent.com/lacostenycoder/cbc380ec7597abf264ff7b79e042f159/raw/049c9f37fdfc9c342045be677289756fc02e9b1d/ruby.vim -O ~/.vim/bundle/vim-ruby/syntax/ruby.vim
+echo "gem 'awesome_print'" >> /app/Gemfile
+bundle install
+clear
+echo "Run tmux for multiplex console goodness!  See: https://duckduckgo.com/?q=tmux+cheat+sheet&atb=v50-5_a&ia=answer&iax=1"
+#rails c
