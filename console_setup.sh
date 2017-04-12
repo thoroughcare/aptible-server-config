d16469d1e2e61933c76c845bd8d6db9648fdd1d8
Lance Jordan <djlancejordan@gmail.com>
2017-04-12 14:07:31 -0400

    Don't install vim plugins on each build
    since we may not need vim each time we ssh to aptible, let's wait on this and run it manually in vim if needed.

--- console_setup.sh
+++ console_setup.sh
@@ -9,9 +9,9 @@ mkdir ~/.vim/colors/
 wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O ~/.vim/colors/molokai.vim
 wget https://raw.githubusercontent.com/thoroughcare/aptible-server-config/master/vimrc -O ~/.vimrc
 # install vim plugins before patching ruby syntax
-vim --cmd '' \
- -c 'PluginInstall' \
- -c 'qa!'
+#vim --cmd '' \
+# -c 'PluginInstall' \
+# -c 'qa!'
 # patched syntax file for vim 7.3 on old linux for server
 wget https://gist.githubusercontent.com/lacostenycoder/cbc380ec7597abf264ff7b79e042f159/raw/049c9f37fdfc9c342045be677289756fc02e9b1d/ruby.vim -O ~/.vim/bundle/vim-ruby/syntax/ruby.vim
 echo "gem 'awesome_print'" >> /app/Gemfile
@@ -19,3 +19,4 @@ bundle install
 clear
 echo "Run tmux for multiplex console goodness!  See: https://duckduckgo.com/?q=tmux+cheat+sheet&atb=v50-5_a&ia=answer&iax=1"
 #rails c
+echo "To install vim pluggins launch vim and type :PlugginInstall"
