93093d810845c01c5188a1a4fa56260270dd0d96
Lance Jordan <djlancejordan@gmail.com>
2018-08-13 09:26:31 -0400

    remove pry junk close thoroughcare/tasks/issues/28

--- console_setup.sh
+++ console_setup.sh
@@ -1,16 +1,3 @@
-
-has_awesome=$(tail Gemfile -n 1 | grep awesome | wc -l)
-
-if (( $has_awesome > 0 )); then
-  echo "awesome_print installed already"
-else
-  echo "gem 'pry-rails'" >> /app/Gemfile
-  echo "gem 'pry-awesome_print'" >> /app/Gemfile
-  echo "gem 'awesome_print'" >> /app/gemfile
-  bundle install
-  clear
-fi
-
 while true; do
     read -p "Do you wish to install dev extras? (vim, tmux, less, dropbox_uploader)" yn
     case $yn in
