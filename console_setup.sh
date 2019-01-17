4da4d193a7693baf874c59094f36a0b8fddc9593
Lance Jordan <djlancejordan@gmail.com>
2019-01-17 13:09:09 -0500

    Stop asking to install extras
    we rarely need this anymore so we can run the script if we need it.

--- console_setup.sh
+++ console_setup.sh
@@ -16,14 +16,5 @@ else
   clear
 fi
 
-while true; do
-    read -p "Do you wish to install dev extras? (vim, tmux, less, dropbox_uploader)" yn
-    case $yn in
-        [Yy]* ) bash <(curl -s https://raw.githubusercontent.com/thoroughcare/aptible-server-config/master/install_extras.sh); break;;
-        [Nn]* ) echo "skipping extras \n"; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-echo "to install extras run this script again with:"
+echo "to install extras(vim, tmux, dropbox utility) run the following:"
 echo "bash <(curl -s https://raw.githubusercontent.com/thoroughcare/aptible-server-config/master/console_setup.sh)"
