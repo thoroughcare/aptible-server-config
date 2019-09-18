8b491bd16920c6cd75eae5f13d510b9a083ed5ff
Lance Jordan <djlancejordan@gmail.com>
2019-09-18 13:26:57 -0400

    fallback to curl if no slack notifier

--- smoke_test.rb
+++ smoke_test.rb
@@ -3,6 +3,10 @@ msg += "\nReading env from server: #{ENV['SERVER_NAME']}"
 if (defind?(SlackNotifier) rescue nil)
   SlackNotifier.new(text: msg, channel: 'server_status').notify
 else
-  puts msg
+code=<<-EOF
+curl -X POST --data-urlencode 'payload={"channel": "#server_status", "username": "Cirlce-CI", "text": "#{msg}"}' #{ENV['SLACK_WEBHOOK']}
+EOF
+  code = code.split("\n").find{|line| line[/urlencode/]}
+  system(code)
 end
 
