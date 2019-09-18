c49fc8d3fb27b2427a9beaffdfceb50048761562
Lance Jordan <djlancejordan@gmail.com>
2019-09-18 12:45:25 -0400

    fix typo in env var

--- smoke_test.rb
+++ smoke_test.rb
@@ -1,5 +1,5 @@
 msg = 'smoke_test run from code within aptible-server-config repo'
-msg += "\nReading env from server: #{ENV['SEVERR_NAME']}"
+msg += "\nReading env from server: #{ENV['SERVER_NAME']}"
 if (defind?(SlackNotifier) rescue nil)
   SlackNotifier.new(text: msg, channel: 'server_status').notify
 else
